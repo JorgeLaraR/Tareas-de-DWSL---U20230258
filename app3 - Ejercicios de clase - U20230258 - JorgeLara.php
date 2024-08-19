78f8c46bff3a23668def76399921d6ef3534835f
Jorge Lara <140337814+JorgeLaraR@users.noreply.github.com>
2024-08-18 21:15:44 -0600

    tarea lista

--- app3 - Ejercicios de clase - U20230258 - JorgeLara.php
+++ app3 - Ejercicios de clase - U20230258 - JorgeLara.php
@@ -16,14 +16,15 @@
                 <th>MORAL</th>
             </tr>
             <tr>
-                <td><input type="number" name="m" id=""></td>
-                <td><input type="number" name="l" id=""></td>
-                <td><input type="number" name="c" id=""></td>
-                <td><input type="number" name="s" id=""></td>
-                <td><input type="number" name="mo" id=""></td>
+                <td><input type="number" name="m" id="" value="<?php echo isset($_POST['m']) ? $_POST['m'] : ''; ?>"></td>
+                <td><input type="number" name="l" id="" value="<?php echo isset($_POST['l']) ? $_POST['l'] : ''; ?>"></td>
+                <td><input type="number" name="c" id="" value="<?php echo isset($_POST['c']) ? $_POST['c'] : ''; ?>"></td>
+                <td><input type="number" name="s" id="" value="<?php echo isset($_POST['s']) ? $_POST['s'] : ''; ?>"></td>
+                <td><input type="number" name="mo" id="" value="<?php echo isset($_POST['mo']) ? $_POST['mo'] : ''; ?>"></td>
             </tr>
         </table>
         <input type="submit" value="Promediar">
+        <input type="submit" name="reset" value="Ingresar nuevo promedio">
     </form>
 </body>
 </html>
